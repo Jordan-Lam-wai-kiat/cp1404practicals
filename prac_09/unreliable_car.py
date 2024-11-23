1381034079259d27e0a3485485aed75b474099f4
JollyPropoganda <74634228+JollyPropoganda@users.noreply.github.com>
2024-11-24 02:44:27 +0800

    shifted Class "UnreliableCar" from "unreliable_car_test.py" to "unreliable_car.py"

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,19 @@
+from car import Car
+
+class UnreliableCar(Car):
+    def __init__(self, reliability = 49, **kwargs):
+        super().__init__(self, **kwargs)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        from random import uniform
+        if self.reliability < uniform(0, 100):
+            if distance > self.fuel:
+                distance = self.fuel
+                self.fuel = 0
+            else:
+                self.fuel -= distance
+            self._odometer += distance
+            return distance
+        else:
+            print("The car didn't start")
--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -1,22 +1,4 @@
-from car import Car
-
-class UnreliableCar(Car):
-    def __init__(self, reliability = 49, **kwargs):
-        super().__init__(self, **kwargs)
-        self.reliability = reliability
-
-    def drive(self, distance):
-        from random import uniform
-        if self.reliability < uniform(0, 100):
-            if distance > self.fuel:
-                distance = self.fuel
-                self.fuel = 0
-            else:
-                self.fuel -= distance
-            self._odometer += distance
-            return distance
-        else:
-            print("The car didn't start")
+from unreliable_car import UnreliableCar
 
 my_car = UnreliableCar()
 my_car.drive(40)
